14758049e9f53e00aeaed17175d6eea9a69c411b
Travis Perdue <perduetravis@gmail.com>
2018-02-25 12:04:34 -0600

    finish chapter 8.3

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,13 @@ enum Cell {
     Text(String)
 }
 
+use std::collections::HashMap;
 
 fn main() {
+    // exploring Vectors in Rust
+
+
+
     // init an empty vector
     let v: Vec<i32> = Vec::new();
     // init a vector with values
@@ -69,7 +74,13 @@ fn main() {
         Cell::Text(String::from("three"))
     ];
 
+
+
+
     // explore Strings in Rust as a collection
+
+
+
 
     let s = String::new();
 
@@ -77,8 +88,8 @@ fn main() {
     // converte to a String like so
     let string_lit = "this is a string";
     let mut s = string_lit.to_string();
-    // strings are UTF-8 by default. all of these are valid
 
+    // strings are UTF-8 by default. all of these are valid
     let hello = String::from("السلام عليكم");
     let hello = String::from("Dobrý den");
     let hello = String::from("Hello");
@@ -92,7 +103,6 @@ fn main() {
     let hello = String::from("Hola");
 
     // some string functions
-
     s.push_str(", I added this with push()");
     s.push('!');
 
@@ -137,4 +147,77 @@ fn main() {
     for byte in "नमस्ते".bytes() {
         println!("{}", byte);
     }
+
+
+
+    // explore Hash maps in Rust
+
+
+
+    // keys must all be of one type
+    // and values must all be of one type
+    // (keys can be different than values) 
+    let mut scores = HashMap::new(); 
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+
+    // A vector of tuples can be transformed into HashMaps
+
+    // type must be explict when using collect to transform because collect
+    // can go to many data types but
+    // HashMap. <_,_> is used because Rust can infer key, val types
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10,50];
+    // zip vectors together into vector of tuples and transform into hash map
+    let scores_from_vectors: HashMap<_,_> = teams.iter().zip(initial_scores.iter()).collect();
+
+
+    // HashMaps take ownership of types that don't implement the Copy trait
+    let key = 10;
+    let value = String::from("Purple");
+
+    let mut map = HashMap::new();
+
+    map.insert(key, value);
+    // map now owns key, value. the program will fail if those
+    // variables are now used.
+    // println!("{:?}", value); => produces moved error
+    // if a ref to a val is in a HashMap, it takes ownership of the reference
+    // but the value itself is not moved into the HashMap
+
+    let key = 10;
+    // get returns an option, Some(&String) in this case
+    let field_value = map.get(&key);
+    match field_value {
+        Some(string) => println!("{:?}", string),
+        _ => (),
+    }
+
+    for (key, value) in &scores {
+        println!("key: {}, value: {}", key, value);
+    }
+
+    // overwrite a value
+    scores.insert(String::from("Blue"), 25) ;
+    // it's that simple!
+    println!("{:?}", scores);
+
+    // insert a value if it currently doesn't exist in the HashMap
+    scores.entry(String::from("Blue")).or_insert(50);
+    scores.entry(String::from("Purple")).or_insert(50);
+    println!("{:?}", scores);
+
+
+    // iterate over a string and return a hash of words as keys
+    // and their count in a string as a value
+    let string_lit = "Hello Rust. I am studying Rust. I am counting words Rust.";
+    let mut map = HashMap::new();
+    for word in string_lit.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
