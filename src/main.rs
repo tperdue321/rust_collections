69449043b5d9c981dfd342ac6f226b4a21256d4b
Travis Perdue <perduetravis@gmail.com>
2018-02-24 20:26:25 -0600

    explore strings with chapt 8.2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+
+#![allow(unused_variables)]
 enum Cell {
     Int(i32),
     Float(f64),
@@ -5,7 +7,6 @@ enum Cell {
 }
 
 
-// #![allow(unused_variables)]
 fn main() {
     // init an empty vector
     let v: Vec<i32> = Vec::new();
@@ -68,4 +69,72 @@ fn main() {
         Cell::Text(String::from("three"))
     ];
 
+    // explore Strings in Rust as a collection
+
+    let s = String::new();
+
+    // any type that implements the Display trait can be
+    // converte to a String like so
+    let string_lit = "this is a string";
+    let mut s = string_lit.to_string();
+    // strings are UTF-8 by default. all of these are valid
+
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שָׁלוֹם");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
+
+    // some string functions
+
+    s.push_str(", I added this with push()");
+    s.push('!');
+
+    let s2: &str = "a &str.";
+    s.push_str(&s2);
+
+
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // Note that s1 has been moved here and can no longer be used
+
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    // this is a bit unwieldy
+    let s = s1 + "-" + &s2 + "-" + &s3;
+    println!("{:?}", s);
+
+    // redeclare s1 variable because it has been moved and is no longer valid
+    let s1 = String::from("tic");
+    // this is a easier to work with
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("{:?}", s);
+
+
+    let hello = "Здравствуйте";
+    // rust cannot do standard indexing because it is UTF-8 encoded
+    // which means 1 byte may not be equivalent to to char
+    let s = &hello[0..4];
+    // &hello[0..1] => this will crash the program because it will cut off
+    // in the middle of the hindi letter З. it is not the same as the english
+    // number 3. slicing must be done with care because it can crash a program
+
+
+    // a safer way to access single chars instead of slicing
+    for ch in "नमस्ते".chars() {
+        println!("{}", ch);
+    }
+    for byte in "नमस्ते".bytes() {
+        println!("{}", byte);
+    }
 }
